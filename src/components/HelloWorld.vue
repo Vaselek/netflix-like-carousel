fb046989723cd3b19b1caf8280f3cc53f032bfde
Vaselek <burzhuas@gmail.com>
2019-02-12 16:31:04 +0600

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,412 @@
+<template>
+  <div class="contain">
+    <h1>Pure CSS Netflix Video Carousel</h1>
+
+    <p>
+      Inspired by  <a href="https://twitter.com/Eli_White">Eli White's</a> article <a href="http://eng.wealthfront.com/2015/06/implementing-netflix-redesign.html">Performant CSS Animations: Netflix Case Study</a>, his <a href="https://codepen.io/TheSavior/pen/LVeYBp">example pen</a>, and <a href="http://matthewjamestaylor.com/">Matt Taylor's</a> <a href="https://codepen.io/mattjamestaylor/pen/dodYPr">CSS-only version.</a>
+    </p>
+
+    <div class="row">
+      <div class="row__inner">
+
+        <div class="tile">
+          <div class="tile__media">
+            <img class="tile__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-1.jpg" alt=""  />
+          </div>
+          <div class="tile__details">
+            <div class="tile__title">
+              Top Gear
+            </div>
+          </div>
+        </div>
+
+        <div class="tile">
+          <div class="tile__media">
+            <img class="tile__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-2.jpg" alt=""  />
+          </div>
+          <div class="tile__details">
+            <div class="tile__title">
+              Top Gear
+            </div>
+          </div>
+        </div>
+
+        <div class="tile">
+          <div class="tile__media">
+            <img class="tile__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-3.jpg" alt=""  />
+          </div>
+          <div class="tile__details">
+            <div class="tile__title">
+              Top Gear
+            </div>
+          </div>
+        </div>
+
+        <div class="tile">
+          <div class="tile__media">
+            <img class="tile__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-4.jpg" alt=""  />
+          </div>
+          <div class="tile__details">
+            <div class="tile__title">
+              Top Gear 123
+            </div>
+            <div>
+              <span class="tile__age">13+</span><span class="tile__seasons">3 seasons</span>
+            </div>
+            <div class="tile__genre">
+              Detective * Triller
+            </div>
+          </div>
+        </div>
+
+        <div class="tile">
+          <div class="tile__media">
+            <img class="tile__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-5.jpg" alt=""  />
+          </div>
+          <div class="tile__details">
+            <div class="tile__title">
+              Top Gear
+            </div>
+          </div>
+        </div>
+
+        <div class="tile">
+          <div class="tile__media">
+            <img class="tile__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-6.jpg" alt=""  />
+          </div>
+          <div class="tile__details">
+            <div class="tile__title">
+              Top Gear
+            </div>
+          </div>
+        </div>
+
+        <div class="tile">
+          <div class="tile__media">
+            <img class="tile__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-7.jpg" alt=""  />
+          </div>
+          <div class="tile__details">
+            <div class="tile__title">
+              Top Gear
+            </div>
+          </div>
+        </div>
+
+        <div class="tile">
+          <div class="tile__media">
+            <img class="tile__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-8.jpg" alt=""  />
+          </div>
+          <div class="tile__details">
+            <div class="tile__title">
+              Top Gear
+            </div>
+          </div>
+        </div>
+
+        <div class="tile">
+          <div class="tile__media">
+            <img class="tile__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-9.jpg" alt=""  />
+          </div>
+          <div class="tile__details">
+            <div class="tile__title">
+              Top Gear
+            </div>
+          </div>
+        </div>
+
+        <div class="tile">
+          <div class="tile__media">
+            <img class="tile__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-10.jpg" alt=""  />
+          </div>
+          <div class="tile__details">
+            <div class="tile__title">
+              Top Gear
+            </div>
+          </div>
+        </div>
+
+        <div class="tile">
+          <div class="tile__media">
+            <img class="tile__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-11.jpg" alt=""  />
+          </div>
+          <div class="tile__details">
+            <div class="tile__title">
+              Top Gear
+            </div>
+          </div>
+        </div>
+
+        <div class="tile">
+          <div class="tile__media">
+            <img class="tile__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-12.jpg" alt=""  />
+          </div>
+          <div class="tile__details">
+            <div class="tile__title">
+              Top Gear
+            </div>
+          </div>
+        </div>
+
+        <div class="tile">
+          <div class="tile__media">
+            <img class="tile__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-13.jpg" alt=""  />
+          </div>
+          <div class="tile__details">
+            <div class="tile__title">
+              Top Gear
+            </div>
+          </div>
+        </div>
+
+        <div class="tile">
+          <div class="tile__media">
+            <img class="tile__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-14.jpg" alt=""  />
+          </div>
+          <div class="tile__details">
+            <div class="tile__title">
+              Top Gear
+            </div>
+          </div>
+        </div>
+
+        <div class="tile">
+          <div class="tile__media">
+            <img class="tile__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-15.jpg" alt=""  />
+          </div>
+          <div class="tile__details">
+            <div class="tile__title">
+              Top Gear
+            </div>
+          </div>
+        </div>
+
+        <div class="tile">
+          <div class="tile__media">
+            <img class="tile__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-16.jpg" alt=""  />
+          </div>
+          <div class="tile__details">
+            <div class="tile__title">
+              Top Gear
+            </div>
+          </div>
+        </div>
+
+        <div class="tile">
+          <div class="tile__media">
+            <img class="tile__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-17.jpg" alt=""  />
+          </div>
+          <div class="tile__details">
+            <div class="tile__title">
+              Top Gear
+            </div>
+          </div>
+        </div>
+
+        <div class="tile">
+          <div class="tile__media">
+            <img class="tile__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-18.jpg" alt=""  />
+          </div>
+          <div class="tile__details">
+            <div class="tile__title">
+              Top Gear
+            </div>
+          </div>
+        </div>
+
+        <div class="tile">
+          <div class="tile__media">
+            <img class="tile__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-19.jpg" alt=""  />
+          </div>
+          <div class="tile__details">
+            <div class="tile__title">
+              Top Gear
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  body,
+  html {
+    padding: 0 10px;
+    margin: 0;
+    background: #0e0f11;
+    color: #ecf0f1;
+    font-family: 'Open Sans', sans-serif;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    width: 100%;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  h1,
+  p {
+    text-align: center;
+  }
+  p {
+    width: 100%;
+    max-width: 500px;
+    margin: auto;
+  }
+  a:link,
+  a:hover,
+  a:active,
+  a:visited {
+    transition: color 150ms;
+    color: #95a5a6;
+    text-decoration: none;
+  }
+  a:hover {
+    color: #7f8c8d;
+    text-decoration: underline;
+  }
+  .contain {
+    width: 100%;
+  }
+  .row {
+    overflow: scroll;
+    width: 100%;
+  }
+  .row__inner {
+    transition: 450ms -webkit-transform;
+    transition: 450ms transform;
+    transition: 450ms transform, 450ms -webkit-transform;
+    font-size: 0;
+    white-space: nowrap;
+    margin: 70.3125px 0;
+    padding-bottom: 10px;
+  }
+  .tile {
+    position: relative;
+    display: inline-block;
+    width: 250px;
+    height: 200px;
+    margin-right: 10px;
+    font-size: 20px;
+    cursor: pointer;
+    transition: 450ms all;
+    -webkit-transform-origin: center left;
+    transform-origin: center left;
+  }
+  .tile__img {
+    width: 250px;
+    height: 200px;
+    -o-object-fit: cover;
+    object-fit: cover;
+  }
+  .tile__details {
+    opacity: 1;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    top: 0;
+    font-size: 10px;
+    opacity: 0;
+    transition: 450ms opacity;
+    background: linear-gradient(to top, rgba(0,0,0,0.9) 0%, rgba(0,0,0,0) 100%);
+  }
+  .tile__details:after,
+  .tile__details:before {
+    content: '';
+    position: absolute;
+    bottom: 41%;
+    left: 4%;
+    display: #000;
+    text-align: left;
+  }
+  .tile__details:after {
+    width: 20px;
+    height: 20px;
+    border: 1px solid #ecf0f1;
+    line-height: 50px;
+    border-radius: 100%;
+    background: rgba(0,0,0,0.5);
+    z-index: 1;
+  }
+  .tile__details:before {
+    content: '▶';
+    /*left: 0;*/
+    width: 100%;
+    font-size: 11px;
+    margin-left: 8px;
+    margin-bottom: 3px;
+    z-index: 2;
+    color: #e50914;
+  }
+  .tile:hover .tile__details {
+    opacity: 1;
+  }
+  .tile__title {
+    position: absolute;
+    bottom: 27%;
+    padding: 10px;
+    color: white;
+    font-weight: bold;
+    font-size: 13px;
+  }
+  .tile__age {
+    display: inline-block;
+    position: absolute;
+    bottom: 23%;
+    padding: 0 2px;
+    color: white;
+    font-weight: bold;
+    border: 1px solid grey;
+    left: 7%;
+    font-size: 10px;
+  }
+  .tile__seasons {
+    display: inline-block;
+    position: absolute;
+    bottom: 23%;
+    padding: 1px 2px;
+    color: white;
+    font-weight: bold;
+    left: 20%;
+    font-size: 10px;
+  }
+  .tile__genre {
+    position: absolute;
+    bottom: 14%;
+    padding: 0px 2px;
+    color: white;
+    font-weight: bold;
+    left: 3%;
+    font-size: 10px;
+  }
+  .row__inner:hover {
+    -webkit-transform: translate3d(-62.5px, 0, 0);
+    transform: translate3d(-62.5px, 0, 0);
+  }
+  .row__inner:hover .tile:hover {
+    -webkit-transform: scale(1.5);
+    transform: scale(1.5);
+    opacity: 1;
+  }
+  .tile:hover ~ .tile {
+    -webkit-transform: translate3d(125px, 0, 0);
+    transform: translate3d(125px, 0, 0);
+  }
+
+</style>
